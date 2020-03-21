fb90b6178536c4a4f9267528d8b4ad14aa03d16d
Alan Semenov <ase@enonic.com>
2020-03-21 12:44:40 +0100

    Finished description of GraphQL queries

--- src/queries/getPersons.js
+++ src/queries/getPersons.js
@@ -1,16 +1,18 @@
 const query = `{
-  query(contentTypes: "%application%:person", query: "valid='true'", first: 100) {
-    name: _name
-    id: _id
-    displayName
-    ... on %application%_Person {
-      data {
-        bio
-        photos {
-          ... on media_Image {
-            url_w800: imageUrl(type: absolute, scale: "width(800)")
-            attachments {
-              name
+  guillotine {
+    query(contentTypes: "%application%:person", query: "valid='true'", first: 100) {
+      name: _name
+      id: _id
+      displayName
+      ... on %application%_Person {
+        data {
+          bio
+          photos {
+            ... on media_Image {
+              url_w800: imageUrl(type: absolute, scale: "width(800)")
+              attachments {
+                name
+              }
             }
           }
         }
